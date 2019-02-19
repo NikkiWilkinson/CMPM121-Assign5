9d4232d55c586d7684b4df471e3c629ebcc20d0b
Dendral <niwilkin@ucsc.edu>
2019-02-18 23:10:09 -0800

    added 4 scripts

---
+++ Assignment 5/Assets/Collectable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public GameObject sparkles;
+    private int counter;
+    public Component triggerEvent; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0; //internal counter
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 90*Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider coin)
+    {
+
+    	if(coin.CompareTag("Player")){
+            
+            //create particles upon pickup
+            Instantiate(sparkles, transform.position, transform.rotation);
+            counter = counter + 1;
+    		
+            //destroy cake when player "collects" it
+    		Destroy(gameObject);
+			Debug.Log("Object destroyed");
+
+    		//when player collides with cake object, add 1 point to score
+    		coin.GetComponent<characterMovement>().points++;
+    	}
+
+        if(counter>=1)
+        {
+            triggerEvent.GetComponent<opendoor>().hasCoins = true;
+            Debug.Log("I have enough coins.");
+        }
+    }
+}
+
+
---
+++ Assignment 5/Assets/characterMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+
+    private float speed = 5;
+    private float turnspeed = 90f;
+    private float rotateAmount;
+
+    public int points = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 1.12f, -28f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    	//enable mouse tracking for first person POV
+    	float translation = Input.GetAxis("Vertical") * speed;
+    	float strafe = Input.GetAxis("Horizontal") * speed;
+    	translation *= Time.deltaTime;
+    	strafe *= Time.deltaTime;
+
+    	transform.Translate(strafe, 0, translation);
+
+
+    	//movement
+        Vector3 direction = Vector3.zero;
+        rotateAmount = turnspeed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.A)) 
+        {
+            transform.Rotate(0, -rotateAmount, 0);
+            //direction.x = 1;
+          //  Debug.Log("Pressed A. I move left!");
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+          //  Debug.Log("Pressed D. I move right!");
+          //  direction.x = -1;
+            transform.Rotate(0, rotateAmount, 0);
+        }
+        if(Input.GetKey(KeyCode.W)) 
+        {
+          //  Debug.Log("Pressed W. I move forward!");
+            direction.z = 1;
+        }
+        if(Input.GetKey(KeyCode.S)) 
+        {
+         //   Debug.Log("Pressed S. I move back!");
+            direction.z = -1;
+        }
+
+        transform.Translate(direction.normalized * Time.deltaTime * speed);
+    }
+
+    //increase points on GUI for player to see
+    private void OnGUI()
+    {
+    	GUI.Label(new Rect(20, 20, 100, 100), "Coins: " + points);
+    }
+}
