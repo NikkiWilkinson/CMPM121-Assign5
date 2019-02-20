3ed2cd17a0328b3f3819c5022dcc838ee87189ba
Dendral <niwilkin@ucsc.edu>
2019-02-19 22:13:16 -0800

    added walking and idle animation loops

--- Assignment 5/Assets/characterMovement.cs
+++ Assignment 5/Assets/characterMovement.cs
@@ -14,7 +14,7 @@ public class characterMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 1.12f, -28f);
+        transform.position = new Vector3(0, 1f, -28f);
 
     }
 
